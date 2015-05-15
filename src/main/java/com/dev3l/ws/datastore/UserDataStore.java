1eca49486918ea584fb9401197fc8306a926d6b5
justin.beall <justin.beall@explorys.com>
2015-05-15 15:59:20 -0400

    Minor updates

--- src/main/java/com/dev3l/ws/datastore/UserDataStore.java
+++ src/main/java/com/dev3l/ws/datastore/UserDataStore.java
@@ -14,9 +14,14 @@ public class UserDataStore {
 
 	private static Map<Long, UserBean> userBeanMap = new LinkedHashMap<Long, UserBean>();
 	static {
-		userBeanMap.put(1l, createUserBean(1, "admin", "admin@email.com", "AC", "admin"));
-		userBeanMap.put(2l, createUserBean(2, "user_one", "user_one@email.com", "AC", "user_one"));
-		userBeanMap.put(3l, createUserBean(3, "user_two", "user_two@email.com", "AC", "user_two"));
+		userBeanMap.put(1l, createUserBean(1, "admin", "admin@email.com", "ACT", "admin"));
+		userBeanMap.put(2l, createUserBean(2, "user_one", "one@email.com", "ACT", "user_one"));
+		userBeanMap.put(3l, createUserBean(3, "user_two", "two@email.com", "ACT", "user_two"));
+		userBeanMap.put(4l, createUserBean(4, "user_three", "three@email.com", "ACT", "user_three"));
+		userBeanMap.put(5l, createUserBean(5, "user_four", "four@email.com", "DEL", "user_four"));
+		userBeanMap.put(6l, createUserBean(6, "user_five", "five@email.com", "ACT", "user_five"));
+		userBeanMap.put(7l, createUserBean(7, "user_six", "six@email.com", "HOL", "user_six"));
+		userBeanMap.put(8l, createUserBean(8, "user_seven", "seven@email.com", "ACT", "user_seven"));
 	}
 
 	private static UserBean createUserBean(final long id, final String userName, final String email, final String status, final String password) {
